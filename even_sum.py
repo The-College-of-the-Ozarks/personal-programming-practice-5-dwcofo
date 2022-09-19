15281b905695c41a357917592015c1882aaca4b3
dwcofo <112203502+dwcofo@users.noreply.github.com>
2022-09-19 14:54:40 -0500

    feat: working

--- even_sum.py
+++ even_sum.py
@@ -1,3 +1,19 @@
 # even_sum.py
 #
 # 
+def cSum (f,l):
+    evens = []
+    lower, higher = f if f <= l else l, l if f <= l else f
+    i = lower+1
+    while i < higher:
+        if i%2 != 0:
+            i+= 1
+            continue    
+        evens.append(i)
+        i += 2
+    print(sum(evens))
+    
+ 
+
+f, l = float(input("What is your first number: ")), float(input("What is your second number: "))   
+cSum(f, l)
--- sqrt.py
+++ sqrt.py
@@ -1,3 +1,17 @@
 # sqrt.py
 #
 # 
+
+def crackSqrt(num, e):
+    
+    x = num/2
+    while abs(x**2 - num) > e:
+        x = (x+num/x)/2
+    return x
+    
+    
+n, epsilon = float(input("What number do you want to find the sqrt of: ")), float(input("How close do you want x to be: "))
+
+x = crackSqrt(n, epsilon)
+
+print("Your number is", n,"and you wanted to be",epsilon,"close.\nMy guess is x which is",x,". When you square it", x**2)
